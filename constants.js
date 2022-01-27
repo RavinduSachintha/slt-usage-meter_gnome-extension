fc2fd6be43e63b1db85756df955643f2461260e0
Sachintha <ravindusachintha53@gmail.com>
2022-01-27 15:05:36 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 // extension constants
 
-var username = "***REMOVED***";
-var password = "***REMOVED***";
+var username = "";
+var password = "";
 var schemaId = "org.gnome.shell.extensions.slt_usage_meter";
 var expiringOffset = 30;
 var iconSize = 16;
