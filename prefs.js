b54f4c226648b4b1af19cbbc56c09180d04e0bfd
RavinduSachintha <ravindusachintha53@gmail.com>
2022-02-01 13:47:24 +0530

    prefs updated

--- prefs.js
+++ prefs.js
@@ -9,42 +9,53 @@ function init() {}
 
 function buildPrefsWidget() {
   const widget = new SltUsageMeterPrefsWidget();
+  // gtk_window_set_default_size(widget,800,600); 
+  gtk_widget_set_size_request(widget,200,300);
   widget.show_all();
   return widget;
 }
 
 const SltUsageMeterPrefsWidget = GObject.registerClass(
-  class MyPrefsWidget extends Gtk.Box {
+  class MyPrefsWidget extends Gtk.ScrolledWindow {
     _init(params) {
       super._init(params);
+      log("Hello")
 
-      this.margin = 20;
-      this.set_spacing(15);
-      this.set_orientation(Gtk.Orientation.VERTICAL);
+      let builder = new Gtk.Builder();
+      builder.set_translation_domain('rs');
+      builder.add_from_file(Me.path + '/prefs.ui');
 
       // this.connect("destroy", Gtk.main_quit);
 
-      let myLabel = new Gtk.Label({
-        label: "Translated Text",
-      });
+      // this.margin = 20;
+      // this.set_spacing(15);
+      // this.set_orientation(Gtk.Orientation.VERTICAL);
 
-      let spinButton = new Gtk.SpinButton();
-      spinButton.set_sensitive(true);
-      spinButton.set_range(-60, 60);
-      spinButton.set_value(0);
-      spinButton.set_increments(1, 2);
+      // // this.connect("destroy", Gtk.main_quit);
 
-      spinButton.connect("value-changed", function (w) {
-        log(w.get_value_as_int());
-      });
+      // let myLabel = new Gtk.Label({
+      //   label: "Translated Text",
+      // });
 
-      let hBox = new Gtk.Box();
-      hBox.set_orientation(Gtk.Orientation.HORIZONTAL);
+      // let spinButton = new Gtk.SpinButton();
+      // spinButton.set_sensitive(true);
+      // spinButton.set_range(-60, 60);
+      // spinButton.set_value(0);
+      // spinButton.set_increments(1, 2);
 
-      hBox.pack_start(myLabel, false, false, 0);
-      hBox.pack_end(spinButton, false, false, 0);
+      // spinButton.connect("value-changed", function (w) {
+      //   log(w.get_value_as_int());
+      // });
 
-      this.add(hBox);
+      // let hBox = new Gtk.Box();
+      // hBox.set_orientation(Gtk.Orientation.HORIZONTAL);
+
+      // hBox.pack_start(myLabel, false, false, 0);
+      // hBox.pack_end(spinButton, false, false, 0);
+
+      // this.add(hBox)
+
+      this.add(builder.get_object('main_prefs'));
     }
   }
 );
