7b561512f94f0a366562ab19eae09be14d171331
RavinduSachintha <ravindusachintha53@gmail.com>
2022-02-02 11:31:34 +0530

    pref window resized

--- prefs.js
+++ prefs.js
@@ -1,6 +1,7 @@
 const Gio = imports.gi.Gio;
 const Gtk = imports.gi.Gtk;
 const GObject = imports.gi.GObject;
+const GLib = imports.gi.GLib;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
@@ -9,9 +10,20 @@ function init() {}
 
 function buildPrefsWidget() {
   const widget = new SltUsageMeterPrefsWidget();
-  // gtk_window_set_default_size(widget,800,600); 
-  gtk_widget_set_size_request(widget,200,300);
   widget.show_all();
+
+  // Access the  main window of prefs
+  GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
+    // In GTK4 (GNOME 40), call `get_root()` instead of `get_toplevel()`
+    let window = widget.get_toplevel();
+    window.resize(400, 200);
+
+    let headerBar = window.get_titlebar();
+    headerBar.title = `${Me.metadata.name} Preferences`;
+
+    return GLib.SOURCE_REMOVE;
+  });
+
   return widget;
 }
 
@@ -19,43 +31,14 @@ const SltUsageMeterPrefsWidget = GObject.registerClass(
   class MyPrefsWidget extends Gtk.ScrolledWindow {
     _init(params) {
       super._init(params);
-      log("Hello")
 
       let builder = new Gtk.Builder();
-      builder.set_translation_domain('rs');
-      builder.add_from_file(Me.path + '/prefs.ui');
+      builder.set_translation_domain("rs");
+      builder.add_from_file(Me.path + "/prefs.ui");
 
       // this.connect("destroy", Gtk.main_quit);
 
-      // this.margin = 20;
-      // this.set_spacing(15);
-      // this.set_orientation(Gtk.Orientation.VERTICAL);
-
-      // // this.connect("destroy", Gtk.main_quit);
-
-      // let myLabel = new Gtk.Label({
-      //   label: "Translated Text",
-      // });
-
-      // let spinButton = new Gtk.SpinButton();
-      // spinButton.set_sensitive(true);
-      // spinButton.set_range(-60, 60);
-      // spinButton.set_value(0);
-      // spinButton.set_increments(1, 2);
-
-      // spinButton.connect("value-changed", function (w) {
-      //   log(w.get_value_as_int());
-      // });
-
-      // let hBox = new Gtk.Box();
-      // hBox.set_orientation(Gtk.Orientation.HORIZONTAL);
-
-      // hBox.pack_start(myLabel, false, false, 0);
-      // hBox.pack_end(spinButton, false, false, 0);
-
-      // this.add(hBox)
-
-      this.add(builder.get_object('main_prefs'));
+      this.add(builder.get_object("main_prefs"));
     }
   }
 );
